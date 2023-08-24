a950852a8969b05a5e423a9e4682b60d5def5cab
Nilakanth Khasaki <59888679+khasakinil@users.noreply.github.com>
2023-08-24 21:54:52 +0530

    Add files via upload

--- BFS_ByAdjacencyMatrix/src/com/bfs/BFSByAdjacencyMatrixMain.java
+++ BFS_ByAdjacencyMatrix/src/com/bfs/BFSByAdjacencyMatrixMain.java
@@ -2,6 +2,6 @@ package com.bfs;
 
 public class BFSByAdjacencyMatrixMain {
 	public static void main(String args[]) {
-		
+		BFSByAdjacencyMatrix adjacencyMatrix = new BFSByAdjacencyMatrix();
 	}
 }
